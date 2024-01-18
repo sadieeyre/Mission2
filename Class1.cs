d9f28c0042eb3a66e164aa9e386b998fb625aebd
sadieeyre <sadieeyre@SADIEEYREAFF7>
2024-01-17 17:34:51 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Mission2
+{
+    internal class RollDice
+    {
+        public int[] DiceRoll(int numRolls)
+        {
+            Random random = new Random();
+            int[] results = new int[13];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                int sum = dice1 + dice2;
+                results[sum]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Mission2;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        RollDice roller = new RollDice();
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        int numRolls = int.Parse(Console.ReadLine());
+
+        int[] results = roller.DiceRoll(numRolls);
+
+        PrintHistogram(results, numRolls);
+    }
+
+    static void PrintHistogram(int[] results, int numRolls)
+    {
+        Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS:\nEach '*' represents 1% of the total number of rolls. \nTotal number of rolls = {numRolls}");
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = (results[i] * 100) / numRolls;
+            Console.Write($"{i}: {new string('*', percentage)}  \n");
+        }
+    }
+}
